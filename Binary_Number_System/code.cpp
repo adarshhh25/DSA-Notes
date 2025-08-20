4252e1fc26f98ba324643af634d62dfeb38e7f1f
adarshhh25 <adarshjha4425@gmail.com>
2025-08-20 15:42:09 +0530

    Changes

--- Binary_Number_System/code.cpp
+++ Binary_Number_System/code.cpp
@@ -1,40 +1,102 @@
-// BInary to Decimal
+// // BInary to Decimal
+
+// #include <iostream>
+// using namespace std;
+
+// void binToDec(int binary) {
+//     int n = binary;
+//     int pow = 1;
+//     int decNum = 0;
+//     while (n > 0) {
+//         int lastDig = n % 10;
+//         decNum += lastDig * pow;
+//         pow *= 2;
+//         n = n /10;
+//     }
+//     cout << decNum << endl;
+// }
+
+// int main () {
+//     binToDec(101);
+//     return 0;
+// }
+
+
+
+// Decimal to Binary Without Stack
 
 #include <iostream>
 using namespace std;
 
-void binToDec(int binary) {
-    int n = binary;
-    int pow = 1;
-    int decNum = 0;
-    while (n > 0) {
-        int lastDig = n % 10;
-        decNum += lastDig * pow;
-        pow *= 2;
-        n = n /10;
-    }
-    cout << decNum << endl;
+void DecimalToBinary (int number) {
+  int n = number;
+  int Binary = 0;
+  int pow = 1;
+  while (n > 0) {
+    int remainder = n % 2;
+    remainder *= pow;
+    Binary += remainder; 
+    pow = pow * 10;
+    n = n / 2;
+  }
+  cout << "The Binary Equivalent is: " << Binary;
 }
 
 int main () {
-    binToDec(101);
-    return 0;
+  DecimalToBinary(5);
+  return 0;
 }
-
 
 
 //Decimal to Binary
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int decToBin (int number) {
-     int n = number;
-}
+// #define SIZE 20  
 
-int main () {
-    int n;
-    cout << "Enter a Number: ";
-    cin >> n;
-    return 0;
-}
+// class Stack {
+// private:
+//     int arr[SIZE];  
+//     int top;       
+
+// public:
+//     Stack() {
+//         top = -1;
+//     }
+
+//     void push(int value) {
+//         if (top == SIZE - 1) {
+//             cout << "Stack Overflow! Cannot push " << value << endl;
+//         } else {
+//             arr[++top] = value;
+//         }
+//     }
+
+//    void display() {
+//     if (top == -1) {
+//         cout << "Stack is empty." << endl;
+//     } else {
+//         cout << "Stack elements (Binary): ";
+//         for (int i = top; i >= 0; i--) {   // FIXED: print from top to bottom
+//             cout << arr[i];
+//         }
+//         cout << endl;
+//     }
+// }
+// };
+
+// // Main function
+// int main() {
+//     Stack s;
+//     int n;
+//     cout <<"Enter a Number: ";
+//     cin >> n;
+//     while (n >= 1) {
+//       int remainder = n % 2;
+//       s.push(remainder);
+//       n = n/2;
+//     };
+//     s.display();
+//     return 0;
+// }
