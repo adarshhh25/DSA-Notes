46d47ff382e1bb6fefe4b6850c4939b140af77c7
adarshhh25 <adarshjha4425@gmail.com>
2025-08-30 22:25:52 +0530

    1D-Arrays Done

--- 12-Arrays-2/code.cpp
+++ 12-Arrays-2/code.cpp
@@ -19,7 +19,7 @@
 
 
 
-//Maximum Subarray Sum
+//Maximum Subarray Sum(Brute Force)
 
 // #include <iostream>
 // #include <climits>
@@ -47,7 +47,8 @@
 
 
 
-//
+// Slightly Optimized
+
 // #include <iostream>
 // #include <climits>
 // using namespace std;
@@ -63,13 +64,129 @@
 //               maxSum = max(maxSum, currSum);
 //            }
 //            else {
-//               currSum += arr[i];
+//               currSum += arr[j];
 //               maxSum = max(maxSum, currSum);
 //            }
 //     }
-//     cout << endl;
 //     i++;
 // }
 //     cout << "The Maximum Subarray sum is : "<<maxSum;
+//     return 0;
+// }
+
+
+
+// Kadanes Algorithm
+
+// #include <iostream>
+// #include <climits>
+// using namespace std;
+
+
+// int main () {
+//     int arr[] = {-2, -3, -6, -5, -4, -1};
+//     int n = sizeof(arr) / sizeof(int);
+//     int currSum = 0;
+//     int maxSum = INT_MIN;
+//     for (int i = 0; i < n; i++) {
+//         currSum += arr[i];
+//         maxSum = max(maxSum, currSum);
+//         if(currSum < 0) {
+//             currSum = 0;
+//         }
+//     }
+//     cout << maxSum;
+//     return 0;
+// }
+
+
+
+//Buy & Sell Stocks
+// My-Approch O(n^2)
+
+// #include <iostream>
+// #include <climits>
+// using namespace std;
+
+// int main () {
+//     int arr[] = {7, 1, 5, 3, 6, 4};
+//     int n = sizeof(arr) / sizeof(int);
+//     int maxProfit = 0;
+//     for(int i = 0; i < n; i++) {
+//         for(int j = i + 1; j < n; j++) {
+//            int  currProfit = arr[j] - arr[i];
+//            maxProfit = max(maxProfit, currProfit);
+//         }
+//     }
+//     cout << maxProfit;
+//     return 0;
+// }
+
+
+
+//Buy & Sell Stocks
+// O(n)
+
+// #include <iostream>
+// #include <climits>
+// using namespace std;
+
+// int main () {
+//     int prices[] = {7, 1, 5, 3, 6, 4};
+//     int n = sizeof(prices) / sizeof(int);
+//     int bestbuy[n];
+//     for(int i = 0; i < n; i++) {
+//        if(i == 0) {
+//         bestbuy[i] = INT_MAX;
+//        } else {
+//         bestbuy[i]= min(bestbuy[i-1], prices[i-1]);
+//        }
+//     }
+//     int maxProfit = 0;
+//     for(int i = 0; i < n; i++) {
+//         int currProfit = prices[i] - bestbuy[i];
+//         maxProfit = max(maxProfit, currProfit);
+//     }
+//     cout<< maxProfit;
+//     return 0;
+// }
+
+
+
+//Traping Rain Water
+
+// #include <iostream>
+// #include <climits>
+// using namespace std;
+
+// void trap(int *heights, int n) {
+//     int leftMax[20000], rightMax[20000];
+//     leftMax[0] = heights[0];
+//     rightMax[n-1] = heights[n-1];
+
+//     for(int i = 1; i < n; i++) {
+//         leftMax[i] = max(leftMax[i-1], heights[i-1]);
+//         // cout << leftMax[i] << ", ";
+//     }
+
+//     for(int i = n-2; i >= 0; i--) {
+//         rightMax[i] = max(rightMax[i+1], heights[i+1]);
+//         // cout << rightMax[i] << ", ";
+//     }
+
+//     int trappedWater = 0;
+//     for(int i = 0; i < n; i++) {
+//         int currWater = min(leftMax[i], rightMax[i]) - heights[i];
+//         if(currWater > 0) {
+//             trappedWater += currWater;
+//         }
+//     }
+//     cout << "Water trapped is: " << trappedWater;
+// }
+
+// int main() {
+//     int heights[] = {4, 2, 0, 6, 3, 2, 5};
+//     int n = sizeof(heights) / sizeof(int);
+//     trap(heights, n);
 //     return 0;
 // }
