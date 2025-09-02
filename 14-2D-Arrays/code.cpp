a3cbc248cc1725c7465d672820e361a0fdc21a96
adarshhh25 <adarshjha4425@gmail.com>
2025-09-02 23:06:52 +0530

    Changes

--- 14-2D-Arrays/code.cpp
+++ 14-2D-Arrays/code.cpp
@@ -1,30 +1,150 @@
+// #include <iostream>
+// using namespace std;
+ 
+// int main () {
+//     int arr[3][3] = {{100, 100, 100}, 
+//                      {89, 82, 88},
+//                      {100, 79, 89}};
+
+//     int new_arr[3][4];
+
+//     for(int i = 0; i < 3; i++) {
+//         for (int j = 0; j < 4; j++) {
+//             cin >> new_arr[i][j];
+//         }
+//     }
+
+//     for(int i = 0; i < 3; i++) {
+//         for(int j = 0; j < 4; j++) {
+//             cout << new_arr[i][j] << ", ";
+//         }
+//         cout << endl;
+//     }    
+
+//     for(int i = 0; i < 3; i++) {
+//         for(int j = 0; j < 3; j++) {
+//             cout << arr[i][j] << ", ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+
+//Spiral Matrix
+
+// #include <iostream>
+// using namespace std;
+
+// void spiralMatrix(int matrix[][4], int m, int n) {
+//     int srow = 0, scol = 0;
+//     int erow = n-1, ecol = m-1;
+    
+//     while (srow <= erow && scol <= ecol) {
+//     //Top
+//     for(int j = scol; j <= ecol; j++) {
+//         cout << matrix[srow][j] << " ";
+//     }
+
+//     //Right
+//     for(int i = srow + 1; i <= erow; i++) {
+//         cout << matrix[i][ecol] << " ";
+//     }
+
+//     //Bottom
+//     for(int j = ecol-1; j >= scol; j--) {
+//         if(srow == erow) {
+//             break;
+//         }
+//         cout << matrix[erow][j] << " ";
+//     }
+
+//     //Left
+//     for(int i = erow-1; i >= srow+1; i--) {
+//         if(scol == ecol) {
+//             break;
+//         }
+//         cout << matrix[i][scol] << " ";
+//     }
+
+//    srow++;
+//    scol++;
+//    erow--;
+//    ecol--;
+//   }
+// }
+
+// int main () {
+//     int matrix[4][4] = {
+//         {1, 2, 3, 4},
+//         {5, 6, 7, 8},
+//         {9, 10, 11, 12},
+//         {13, 14, 15, 16}
+//     }; 
+//     spiralMatrix(matrix, 4, 4);
+//     return 0;
+// }
+
+
+
+//Diagonal Sum
+
+// #include <iostream>
+// using namespace std;
+
+// int main () {
+//     int matrix[3][3] = {
+//         {1, 2, 3},
+//         {5, 6, 7},
+//         {9, 10, 11}
+//     }; 
+
+//     int sum = 0;
+
+//     // for(int i = 0; i < 3; i++) {
+//     //   for(int j = 0; j < 3; j++) {
+//     //     if(i == j) {
+//     //       sum += matrix[i][j];
+//     //     }
+//     //     else if(j == 3-i-1){
+//     //       sum += matrix[i][j];
+//     //     }
+//     //   }
+//     // }
+  
+//     for(int i = 0; i < 3; i++) {
+//         sum += matrix[i][i];
+//         if(i != 3-i-1) {
+//            sum += matrix[i][3-i-1];
+//         }
+//     }
+
+//     cout << sum; 
+//     return 0;
+// }
+
+
+
+//Search in Sorted Matrix
+
 #include <iostream>
 using namespace std;
- 
+
 int main () {
-    int arr[3][3] = {{100, 100, 100}, 
-                     {89, 82, 88},
-                     {100, 79, 89}};
-
-    int new_arr[3][4];
-
-    for(int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++) {
-            cin >> new_arr[i][j];
-        }
-    }
-
-    for(int i = 0; i < 3; i++) {
+    int matrix[4][4] = {
+        {10, 20, 30, 40},
+        {15, 25, 35, 45},
+        {27, 29, 37, 48},
+        {32, 33, 39, 50}
+    };
+    int key = 303;
+    for(int i = 0; i < 4; i++) {
         for(int j = 0; j < 4; j++) {
-            cout << new_arr[i][j] << ", ";
+            if(key == matrix[i][j]) {
+                cout << "Key Found At " << i << ", " << j;
+            }
         }
-        cout << endl;
-    }    
-
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            cout << arr[i][j] << ", ";
-        }
-        cout << endl;
     }
+    cout << "Key is not there in the Matrix";
+    return 0;
 }
