73a097515b7ca0c0d01e21c216c5d41eb7486b34
adarshhh25 <adarshjha4425@gmail.com>
2025-12-06 20:40:09 +0530

    Add cycle detection and removal methods for linked lists

--- 20-LinkedList/code.cpp
+++ 20-LinkedList/code.cpp
@@ -203,7 +203,7 @@
 
 
 
-///Detect a Cycle in Linked List
+///Detect a Cycle in Linked List (141)
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -227,3 +227,328 @@
 //         return false;
 //     }
 // };
+
+
+
+//Return Node where cycle starts (142)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+//         bool isCycle = false;
+//         while(fast != NULL && fast->next != NULL) {
+//             slow = slow->next;
+//             fast = fast->next->next;
+//             if(slow == fast) {
+//                 isCycle = true;
+//                 break;
+//             }
+//         }
+
+//         if(!isCycle) {
+//          return NULL;
+//         }
+
+//         slow = head;
+//         while(slow != fast) {
+//             slow = slow->next;
+//             fast = fast->next;
+//         }
+//         return slow;
+//     }
+// };
+
+
+
+//Remove Cycle 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+//         bool isCycle = false;
+//         while(fast != NULL && fast->next != NULL) {
+//             slow = slow->next;
+//             fast = fast->next->next;
+//             if(slow == fast) {
+//                 isCycle = true;
+//                 break;
+//             }
+//         }
+
+//         if(!isCycle) {
+//          return NULL;
+//         }
+
+//         slow = head;
+//         LisNode* prev = NULL;
+//         while(slow != fast) {
+//             slow = slow->next;
+//             prev = fast;
+//             fast = fast->next;
+//         }
+//         prev->next = NULL;
+//         return slow;
+//     }
+// };
+
+
+
+///Doubly Linked List
+// #include <iostream>
+// using namespace std;
+
+// class Node {
+//     public:
+//        int data;
+//        Node* prev;
+//        Node* next;
+    
+//     Node(int val) {
+//         data = val;
+//         prev = next = NULL;
+//     }
+// };
+
+// class DoublyList {
+//     Node* head;
+//     Node* tail;
+
+//     public:
+//        DoublyList() {
+//         head = tail = NULL;
+//        }
+
+//       void push_front(int val) {
+//         Node* newNode = new Node(val);
+//         if(head == NULL) {
+//             head = tail = newNode;
+//         }
+//         else {
+//             newNode->next = head;
+//             head->prev = newNode;
+//             head = newNode;
+//         }
+//       }
+
+//       void push_back(int val) {
+//         Node* newNode = new Node(val);
+//         if(head == NULL) {
+//             head = tail = newNode;
+//         }
+//         else {
+//             newNode->prev = tail;
+//             tail->next = newNode;
+//             tail = newNode;
+//         }
+//       }
+
+//       void pop_front() {
+//        if(head == NULL) {
+//         cout << "List is Empty";
+//         return;
+//        }
+//        Node* temp = head;
+//        head = head->next;
+//        if(head != NULL) {
+//         head->prev = NULL;
+//        }
+//        temp->next = NULL;
+//        delete temp;
+//       }
+
+//       void pop_back() {
+//        if(head == NULL) {
+//         cout << "List is Empty";
+//         return;
+//        }
+//        Node* temp = tail;
+//        tail = tail->prev;
+//        if(tail != NULL) {
+//          tail->next = NULL;
+//        }
+//        temp->prev = NULL;
+//        delete temp;
+//       }
+
+//       void print () {
+//         Node* temp = head;
+//         while(temp != NULL) {
+//             cout<< temp->data << "<=>";
+//             temp = temp->next;
+//         }
+//         cout << "NULL\n";
+//       }
+
+// };
+
+// int main () {
+//     DoublyList dll;
+//     dll.push_front(1);
+//     dll.push_front(2);
+//     dll.push_front(3);
+
+//     dll.print();
+
+//     dll.push_back(4);
+//     dll.push_back(5);
+//     dll.push_back(6);
+
+//     dll.print();
+
+//     dll.pop_front();
+
+//     dll.print();
+
+//     dll.pop_back();
+
+//     dll.print();
+//     return 0;
+// }
+
+
+
+///Circular Linked List
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+       int data;
+       Node* next;
+
+    Node(int val) {
+       data = val;
+       next = NULL;
+    }
+};
+
+class CircularList {
+    Node* head;
+    Node* tail;
+
+    public:
+
+    CircularList() {
+        head = tail = NULL;
+    }
+
+    void inserAtHead(int val) {
+      Node* newNode = new Node(val);
+      if(head == NULL) {
+        head = tail = newNode;
+        tail->next = head;
+      }
+      else {
+        newNode->next = head;
+        head = newNode;
+        tail->next = newNode;
+      }
+    }
+
+    void insertAtTail(int val) {
+      Node* newNode = new Node(val);
+      if(head == NULL) {
+        head = tail = newNode;
+        tail->next = head;
+      }
+      else {
+        newNode->next = head;
+        tail->next = newNode;
+        tail = newNode;        
+      }
+    }
+
+    void deleteAtHead() {
+       if(head == NULL) {
+        return;
+       }
+       else if(head == tail) {
+           delete head;
+           head = tail = NULL;
+       }
+       else {
+         Node* temp = head;
+         head = head->next;
+         tail->next = head;
+         temp->next = NULL;
+         delete temp;
+       }
+    }
+
+    void deleteAtTail() {
+      if(head == NULL) {
+        return;
+       }
+       else if(head == tail) {
+           delete head;
+           head = tail = NULL;
+       }
+      else {
+         Node* temp =  tail;
+         Node* prev = head;
+         while(prev->next != tail) {
+           prev = prev->next;
+         }
+         tail = prev;
+         tail->next = head;
+         temp->next = NULL;
+         delete temp;
+      }
+    }
+
+    void print() {
+      if(head == NULL) return;
+
+      cout << head->data << "->";
+      Node* temp = head->next;
+      while(temp != head) {
+        cout << temp->data << "->";
+        temp = temp->next;
+      }
+      cout << temp->data << endl;
+    }
+    
+};
+
+int main () {
+    CircularList cll;
+    
+    cll.inserAtHead(1);
+    cll.inserAtHead(2);
+    cll.inserAtHead(3);
+
+    cll.print();
+
+    cll.insertAtTail(4);
+    cll.insertAtTail(5);
+    cll.insertAtTail(6);
+
+    cll.print();
+
+    cll.deleteAtHead();
+
+    cll.print();
+
+    cll.deleteAtTail();
+
+    cll.print();
+    return 0;
+}
