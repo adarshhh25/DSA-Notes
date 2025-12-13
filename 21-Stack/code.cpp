c04405fbba089ec41bcd2ec26c7e21cbfcb7a8d7
adarshhh25 <adarshjha4425@gmail.com>
2025-12-13 17:50:56 +0530

    Add various stack-related implementations and examples in code.cpp

--- 21-Stack/code.cpp
+++ 21-Stack/code.cpp
@@ -182,34 +182,193 @@
 // }
 
 
+//901
+// #include <iostream>
+// #include <vector>
+// #include <stack>
+// using namespace std;
 
-#include <iostream>
-#include <vector>
-#include <stack>
-using namespace std;
+// int main () {
+//     vector<int> price = {100, 80, 60, 70, 60, 75, 85};
+//     stack<int> s;
+//     vector<int> ans(price.size(), 0);
 
-int main () {
-    vector<int> price = {100, 80, 60, 70, 60, 75, 85};
-    stack<int> s;
-    vector<int> ans(price.size(), 0);
+//     for(int i = 0; i < price.size(); i++) {
+//         while(s.size() > 0 && price[s.top()] <= price[i]) {
+//             s.pop();
+//         }
 
-    for(int i = 0; i < price.size(); i++) {
-        while(s.size() > 0 && price[s.top()] <= price[i]) {
-            s.pop();
-        }
+//         if(s.empty()) {
+//           ans[i] = i + 1;
+//         }
+//         else {
+//             ans[i] = i - s.top();
+//         }
+//         s.push(i);
+//     }
 
-        if(s.empty()) {
-          ans[i] = i + 1;
-        }
-        else {
-            ans[i] = i - s.top();
-        }
-        s.push(i);
-    }
+//      for(int val : ans) {
+//             cout << val << " ";
+//      }
 
-     for(int val : ans) {
-            cout << val << " ";
-     }
+//     return 0;
+// }
 
-    return 0;
-}
+
+
+///Next Greater Element Approach
+// #include <iostream>
+// #include <vector>
+// #include <stack>
+// using namespace std;
+
+// int main () {
+//     vector<int> arr = {6, 8, 2, 1, 3};
+//     vector<int> nextgreaterElement(arr.size(), 0);
+//     stack<int> s;
+//     int n = arr.size();
+//     for(int i = n-1; i >= 0; i--) {
+
+//     while(s.size () > 0 &&  s.top() <= arr[i]) {
+//         s.pop();
+//     }
+
+//     if(s.empty()) {
+//         nextgreaterElement[i] = -1;
+//     } else {
+//         nextgreaterElement[i] = s.top();
+//     }
+
+//     s.push(arr[i]);
+//     }
+
+//     for(int val : nextgreaterElement) {
+//         cout << val << " ";
+//     }
+//     cout << endl;
+
+//     return 0;
+// }
+
+
+///496
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//         unordered_map<int, int> m;
+//         vector<int> ans;
+//         stack<int> s;
+
+//         for(int i = nums2.size()-1; i >= 0; i--) {
+//             while(s.size() > 0 && s.top() < nums2[i]) {
+//                 s.pop();
+//             }
+//             if(s.empty()) {
+//                m[nums2[i]] = -1;
+//             }
+//             else {
+//                 m[nums2[i]] = s.top();
+//             }
+//             s.push(nums2[i]);
+//         }
+
+//         for(int i = 0; i < nums1.size(); i++) {
+//            ans.push_back(m[nums1[i]]);
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+///Previous Smaller Element
+// #include<iostream>
+// #include <vector>
+// #include <stack>
+// using namespace std;
+
+// int main () {
+//     vector<int> arr = {3, 1, 0, 8, 6};
+//     vector<int> ans(arr.size(), 0);
+//     stack<int> s;
+//     int n = arr.size() - 1;
+//     for(int i = 0; i <= n; i++) {
+//         while(s.size() > 0 && s.top() >= arr[i]) {
+//             s.pop();
+//         }
+//         if(s.empty()) {
+//             ans[i] = -1;
+//         } else {
+//             ans[i] = s.top();
+//         }
+//         s.push(arr[i]);
+//     }
+
+//     for(int val : ans) {
+//         cout << val << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
+
+// 155 - With SC(2n)
+// class MinStack {
+// public:
+//     stack<pair<int, int>> s;
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         if(s.empty()) {
+//             s.push({val, val});
+//         }
+//         else {
+//             int minVal = min(val, s.top().second);
+//             s.push({val, minVal});
+//         }
+//     }
+    
+//     void pop() {
+//         s.pop();
+//     }
+    
+//     int top() {
+//         return s.top().first;
+//     }
+    
+//     int getMin() {
+//         return s.top().second;
+//     }
+// };
+
+//// 155 with SC(n)
+// class MinStack {
+// public:
+//     stack<pair<int, int>> s;
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         if(s.empty()) {
+//             s.push({val, val});
+//         }
+//         else {
+//             int minVal = min(val, s.top().second);
+//             s.push({val, minVal});
+//         }
+//     }
+    
+//     void pop() {
+//         s.pop();
+//     }
+    
+//     int top() {
+//         return s.top().first;
+//     }
+    
+//     int getMin() {
+//         return s.top().second;
+//     }
+// };
